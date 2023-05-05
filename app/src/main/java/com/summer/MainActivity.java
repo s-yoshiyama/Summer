4ef1a821db4c8b675071e060326807d63ed8e0c3
吉山大翔 <yoshiyama@yoshiyama.local>
2023-05-05 10:30:23 +0900

    リファクタリング完了

--- app/src/main/java/com/summer/MainActivity.java
+++ app/src/main/java/com/summer/MainActivity.java
@@ -2,129 +2,102 @@ package com.summer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.summer.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(this.getLayoutInflater());
+        setContentView(binding.getRoot());
+        this.setInputKeyClickListeners(binding);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setInputKeyClickListeners();
     }
 
     /**
      * 入力箇所を押下した時のイベントを設定する。
      */
-    private void setInputKeyClickListeners() {
-        this.findViewById(R.id.zero).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.zero);
-            textView.setText(currentText);
+    @SuppressLint("SetTextI18n")
+    private void setInputKeyClickListeners(ActivityMainBinding binding) {
+        binding.zero.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.zero);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.one).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.one);
-            textView.setText(currentText);
+        binding.one.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.one);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.two).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.two);
-            textView.setText(currentText);
+        binding.two.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.two);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.three).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.three);
-            textView.setText(currentText);
+        binding.three.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.three);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.four).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.four);
-            textView.setText(currentText);
+        binding.four.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.four);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.five).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.five);
-            textView.setText(currentText);
+        binding.five.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.five);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.six).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.six);
-            textView.setText(currentText);
+        binding.six.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.six);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.seven).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.seven);
-            textView.setText(currentText);
+        binding.seven.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.seven);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.eight).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.eight);
-            textView.setText(currentText);
+        binding.eight.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.eight);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.nine).setOnClickListener((listener) -> {
-            TextView textView = this.findViewById(R.id.current_area);
-            String currentText = textView.getText().toString();
-            currentText = currentText + this.getString(R.string.nine);
-            textView.setText(currentText);
+        binding.nine.setOnClickListener(listener -> {
+            String setText = binding.currentArea.getText().toString() + this.getResources().getString(R.string.nine);
+            binding.currentArea.setText(setText);
         });
-        this.findViewById(R.id.clear).setOnClickListener((listener) -> {
-            this.resetCurrentArea();
+        binding.clear.setOnClickListener(listener -> binding.currentArea.setText(""));
+        binding.enter.setOnClickListener(listener -> {
+            String currentText = binding.currentArea.getText().toString();
+            if (!currentText.equals("")) {
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                TextView textView = new TextView(this);
+                textView.setText(currentText);
+                textView.setGravity(Gravity.END);
+                textView.setOnClickListener((textViewListener) -> {
+                    binding.currentArea.setText(currentText);
+                    binding.decidedArea.removeView(textViewListener);
+                });
+                textView.setTextSize(40);
+                textView.setBackground(this.getDrawable(R.drawable.input_key));
+                binding.decidedArea.addView(textView, layoutParams);
+                binding.currentArea.setText("");
+            }
         });
-        this.findViewById(R.id.enter).setOnClickListener((listener) -> {
-            LinearLayout linearLayout = this.findViewById(R.id.decided_area);
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            TextView currentTextView = this.findViewById(R.id.current_area);
-            String currentText = currentTextView.getText().toString();
-            TextView textView = new TextView(this);
-            textView.setText(currentText);
-            textView.setGravity(Gravity.RIGHT);
-            textView.setOnClickListener((textViewListener) -> {
-                if (currentText != "" || currentText != null) {
-                    TextView currentArea = this.findViewById(R.id.current_area);
-                    currentArea.setText(currentText);
-                    linearLayout.removeView(textView);
-                }
-            });
-            textView.setTextSize(40);
-            textView.setBackground(this.getDrawable(R.drawable.input_key));
-            linearLayout.addView(textView, layoutParams);
-            this.resetCurrentArea();
-        });
-        this.findViewById(R.id.calc).setOnClickListener((listener) -> {
-            LinearLayout linearLayout = this.findViewById(R.id.decided_area);
-            int childCount = linearLayout.getChildCount();
-            Integer sum = 0;
+        binding.calc.setOnClickListener(listener -> {
+            int childCount = binding.decidedArea.getChildCount();
+            int sum = 0;
             for(int i = 0; i < childCount; i++) {
-                TextView textView = (TextView) linearLayout.getChildAt(i);
+                TextView textView = (TextView) binding.decidedArea.getChildAt(i);
                 sum += Integer.parseInt(textView.getText().toString());
             }
-            TextView textView = this.findViewById(R.id.current_area);
-            textView.setText(sum.toString());
+            binding.currentArea.setText(Integer.toString(sum));
         });
-    }
-
-    private void resetCurrentArea() {
-        TextView textView = this.findViewById(R.id.current_area);
-        textView.setText("");
     }
 
 }
---
+++ app/src/main/java/com/summer/MainActivityViewModel.java
@@ -0,0 +1,16 @@
+package com.summer;
+
+import androidx.lifecycle.ViewModel;
+
+import com.summer.databinding.ActivityMainBinding;
+
+public class MainActivityViewModel extends ViewModel {
+
+    /**
+     * コンストラクタ。
+     */
+    public MainActivityViewModel() {
+        super();
+    }
+
+}
